9ae7c7aafe3eb714fcc5971685b5e53b581d9f0d
ashwani2 <ashwani.kumar@1406@gmail.com>
2023-02-04 12:25:29 +0530

    error fix

--- nativeServer.js
+++ nativeServer.js
@@ -3,5 +3,5 @@ const webSocket=require("ws")
 const server=http.createServer((req,res)=>{
     res.end("I am connected")
 })
-const wss=new webSocket.WebSocket({server})
+const wss=new webSocket.WebSocketServer({server})
 server.listen(8000)
